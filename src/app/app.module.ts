cb1534fb585bdb7ba2c0c1df14f73413bc29566c
Rabindranath Ferreira <ferreiralox@gmail.com>
2020-05-08 17:01:41 -0400

    config devTools and check loading state loaded sucess

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,8 @@ import { AuthService } from './services/auth/auth.service';
 
 // ngrx settings
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { appReducers } from './app.reducer';
 
 @NgModule({
@@ -42,7 +44,8 @@ import { appReducers } from './app.reducer';
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule, // imports firebase/storage only needed for storage features
-    StoreModule.forRoot( appReducers )
+    StoreModule.forRoot( appReducers ), // multi reducers configuration
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }) // add redux dev tools
   ],
   providers: [ AuthService ],
   bootstrap: [AppComponent]
